f71ecf982d60ed9dd360c4a968852edd33a30b8d
akashrajkr <aakashrajkr@gmail.com>
2020-04-08 15:52:18 +0530

    Removed default output model path.

--- predictor.py
+++ predictor.py
@@ -1,15 +1,17 @@
 import argparse
 from trainer import MultiClassPerceptron
 
+OUTPUT_PATH = 'output/'
 
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-t", "--test_image",
                     help="path to input directory of images")
-    ap.add_argument('-m', "--model", default='output/bike_classifier',
+    ap.add_argument('-m', "--model", required=True,
                     help="Train the model with specified number of epochs")
     args = vars(ap.parse_args())
-    classifier_path = args['model']
+    classifier_name = args['model']
+    classifier_path = OUTPUT_PATH + classifier_name
     classifier = MultiClassPerceptron.load_classifier(classifier_path)
     if args['test_image']:
         test_image_path = args['test_image']
